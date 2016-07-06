d8b3f7b24c056d800f1d91453478be0d23481e3d
vagrant <vagrant@mthocsemr>
2016-07-06 13:58:21 +0000

    20160705 - Update Patient Table

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -32,13 +32,13 @@ $factory->define(App\MthPatient::class, function (Faker\Generator $faker) {
     
     return [
         'name'          => $faker->name,
-        'id_num'        => $faker->regexify('[0-9]{2}(0[1-9]|1[012])(0[1-9]|1[0-9]|2[0-9]|3[01])-?[012349][0-9]{6}'),
+        'id_num'        => $faker->regexify('[0-9]{2}(0[1-9]|1[012])(0[1-9]|1[0-9]|2[0-9]|3[01])-[012349][0-9]{6}'),
         'birthday'      => $faker->date('Ymd'),
         'chk_solar'     => $faker->boolean,
-        'buss_num'      => $faker->regexify('([0-9]{3})-?([0-9]{2})-?([0-9]{5})'),
+        'buss_num'      => $faker->regexify('([0-9]{3})-([0-9]{2})-([0-9]{5})'),
         'buss_nam'      => $faker->company,
         'phone'         => $faker->regexify('^0(2|3[1-3]|4[1-4]|5[1-5]|6[1-4])-(\d{3,4})-(\d{4})$'),
-        'cellphone'     => $faker->regexify('^01[0|1|6|7|8|9]-([0-9]{3,4})-([0-9]{4})$'),
+        'cellphone'     => $faker->regexify('^01(0|1|6|7|8|9)-([0-9]{3,4})-([0-9]{4})$'),
         'email'         => $faker->email,
         'dis_id'        => $faker->randomElement($disCodes),
         'info_agg'      => $faker->boolean,
--- database/migrations/2016_07_06_102154_create_mth_patients_table.php
+++ database/migrations/2016_07_06_102154_create_mth_patients_table.php
@@ -15,16 +15,16 @@ class CreateMthPatientsTable extends Migration
         Schema::create('mth_patients', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 40);         //수진자명
-            $table->string('id_num', 50);       //주민번호 (XXXXXX-XXXXXXX) - 암호화/validation
+            $table->string('id_num', 1000);     //주민번호 (XXXXXX-XXXXXXX) - 암호화/validation
             $table->string('birthday', 8);      //생년월일 (780517 형식) - validation
             $table->boolean('chk_solar');       //양력
-            $table->string('buss_num', 50)
+            $table->string('buss_num', 1000)
                   ->nullable();                 //사업자등록번호(XXX-XX-XXXXX 형태) - 암호화/validation
             $table->string('buss_nam', 50)
                   ->nullable();                 //사업자명
-            $table->string('phone', 20)
+            $table->string('phone', 1000)
                   ->nullable();                 //전화번호 (00-000-0000 형태로 저장) - validation
-            $table->string('cellphone', 20)
+            $table->string('cellphone', 1000)
                   ->nullable();                 //휴대폰번호 (000-000-0000 형태로 저장) - validation
             $table->string('email', 50);        //이메일주소 - validation
             $table->integer('dis_id')
@@ -40,9 +40,11 @@ class CreateMthPatientsTable extends Migration
             $table->string('addr', 50);         //주소
             $table->string('addr_detail', 100); //상세주소
             $table->integer('vet_id')
-                  ->unsigned();                 //보훈구분키
+                  ->unsigned()
+                  ->nullable();                 //보훈구분키
             $table->foreign('vet_id')->references('id')->on('mth_codes');
-            $table->string('vet_num', 30);      //보훈번호 ->validation
+            $table->string('vet_num', 30)
+                  ->nullable()           ;      //보훈번호 ->validation
             $table->text('memo')->nullable();   //메모
             $table->timestamps();
         });
